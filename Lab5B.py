38153a76bdd4afed7fec1cb3d733b2821ff33639
amoora <omardiab.m@gmail.com>
2025-02-09 15:26:41 -0500

    spiderman

--- Lab5B.py
+++ Lab5B.py
@@ -1,16 +1,18 @@
 stars = int(input("Please enter a value for the size: "))
 
+print(f" This is the requested {stars}x{stars} box:")
 for shape in range(stars):
     for n in range(stars):
         print("*", end="")
-    print("")
+    print()
 
-
-for shape in range(stars+1):
+print(f"This is the requested right-facing {stars}x{stars} right-triangle:")
+for shape in range(1,stars+1):
     for x in range(shape):
         print(f"", end="*")
-    print("")
+    print()
 
+print(f"This is the requested left-facing {stars}x{stars} right-triangle:")
 for shape in range(1, stars + 1):
     for x in range(stars - shape):
         print(" ", end="")
