17060b63a2b0cb0ecfe2931345406e4a02f59b6c
amoora <omardiab.m@gmail.com>
2025-02-09 15:31:24 -0500

    spiderman

--- Lab5A.py
+++ Lab5A.py
@@ -1,8 +1,8 @@
+print("Please enter 10 numbers and this program will display the largest.")
 largest = 0
 for number in range(1,11):
-    usrin = int(input(f"please enter number {number}: "))
-    print()
+    usrin = int(input(f"Please enter number {number}: "))
     if usrin > largest:
         largest = usrin
-
+    print("")
 print(f"The largest number was {largest}")
